607488a5c1db59efc8cfc7cdf79c037e9525f27c
dariogguillen <dar_fe@hotmail.com>
2018-01-26 16:42:14 -0600

    modificando js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(() => {
-    const $contenedor = $('#container');
-    const $contenedorImg = $('#containerImg');
-    const $botonPeliculas = $('#films');
-    // plantilla que se agregara al HTML al obtener la respuesta de la api
-    const plantilla = `
+  const $contenedor = $('#container')
+  const $contenedorImg = $('#containerImg')
+  const $botonPeliculas = $('#films')
+  // plantilla que se agregara al HTML al obtener la respuesta de la api
+  const plantilla = `
             <div class='carta '>
                 <div class='info'>
                     <h2>Nombre de la Pelicula: <span>:title:</span></h2>
@@ -15,57 +15,68 @@ $(() => {
                 <div class='image'>
                     <img src="img/peliculas/:img:.jpg" alt="Imagen :img:">
                 </div>
-            </div>`;
+            </div>`
 
-    //arreglo con los nombres de las imagenes que se encuentran en ./img/peliculas/
-    const imgPeliculas = [
-        'A New Hope',
-        'Attack of the Clones',
-        'The Phantom Menace',
-        'Revenge of the Sith',
-        'Return of the Jedi',
-        'The Empire Strikes Back',
-        'The Force Awakens'
-    ];
+  //arreglo con los nombres de las imagenes que se encuentran en ./img/peliculas/
+  const imgPeliculas = [
+    'A New Hope',
+    'Attack of the Clones',
+    'The Phantom Menace',
+    'Revenge of the Sith',
+    'Return of the Jedi',
+    'The Empire Strikes Back',
+    'The Force Awakens'
+  ]
 
-    // Funcion para iterar imgPeliculas
-    const insertarImg = tituloPelicula => {
-        const length = imgPeliculas.length;
-        for (let i = 0; i < length; i++) {
-            /******************************************************************************1
+  // Funcion para iterar imgPeliculas
+  const insertarImg = tituloPelicula => {
+    const length = imgPeliculas.length
+    for (let i = 0; i < length; i++) {
+      /******************************************************************************1
             Mostrando en consola el valor de estos elementos.
             En cada iteracion muestra i=0 y el primer valor de imgPeliculas,
             cuando deberia de mostrar valores diferentes ya que se supone i esta cambiando.
             ******************************************************************************/
-            console.log(i, imgPeliculas[i]);
-            if (imgPeliculas[i] === tituloPelicula) {
-                //solo se cumple para la primera iteracion del for por lo que para las demas regresa lo que tiene else
-                return imgPeliculas[i];
-            } else {
-                return 'Star-Wars';
-            }
-        }
-    };
+      console.log(i, imgPeliculas[i])
+      if (imgPeliculas[i] === tituloPelicula) {
+        return imgPeliculas[i]
+      } else if (imgPeliculas[i + 1] === tituloPelicula) {
+        return imgPeliculas[i + 1]
+      } else if (imgPeliculas[i + 2] === tituloPelicula) {
+        return imgPeliculas[i + 2]
+      } else if (imgPeliculas[i + 3] === tituloPelicula) {
+        return imgPeliculas[i + 3]
+      } else if (imgPeliculas[i + 4] === tituloPelicula) {
+        return imgPeliculas[i + 4]
+      } else if (imgPeliculas[i + 5] === tituloPelicula) {
+        return imgPeliculas[i + 5]
+      } else if (imgPeliculas[i + 6] === tituloPelicula) {
+        return imgPeliculas[i + 6]
+      } else {
+        return 'Star-Wars'
+      }
+    }
+  }
 
-    // Al dar click en el boton#films se ejecuta esta funcion
-    const pedido = () => {
-        $.ajax('https://swapi.co/api/films/').then(peliculas => {
-            const renderPeliculas = peliculas => {
-                peliculas.results.forEach(pelicula => {
-                    const contenedorPelicula = plantilla // Sustituyendo valores en la plantilla
-                        .replace(':title:', pelicula.title)
-                        .replace(':episode:', pelicula.episode_id)
-                        .replace(':date:', pelicula.release_date)
-                        .replace(':director:', pelicula.director)
-                        .replace(':opening:', pelicula.opening_crawl)
-                        .replace(':img:', insertarImg(pelicula.title)); // llamando la funcion que itera el arreglo imgPeliculas
+  // Al dar click en el boton#films se ejecuta esta funcion
+  const pedido = () => {
+    $.ajax('https://swapi.co/api/films/').then(peliculas => {
+      const renderPeliculas = peliculas => {
+        peliculas.results.forEach(pelicula => {
+          const contenedorPelicula = plantilla // Sustituyendo valores en la plantilla
+            .replace(':title:', pelicula.title)
+            .replace(':episode:', pelicula.episode_id)
+            .replace(':date:', pelicula.release_date)
+            .replace(':director:', pelicula.director)
+            .replace(':opening:', pelicula.opening_crawl)
+            .replace(':img:', insertarImg(pelicula.title)) // llamando la funcion que itera el arreglo imgPeliculas
 
-                    $contenedorImg.hide(); // escondiendo el elemento #containerImg
-                    $contenedor.append(contenedorPelicula); //agregando la plantilla con las sustituciones en #container
-                });
-            };
-            renderPeliculas(peliculas); // Ejecucion de la funcion que se encargar de hacer cambios en la plantilla y agregar elementos al dom
-        });
-    };
-    $botonPeliculas.click(pedido); // Evento que ejecuta la funcion pedido
-});
+          $contenedorImg.hide() // escondiendo el elemento #containerImg
+          $contenedor.append(contenedorPelicula) //agregando la plantilla con las sustituciones en #container
+        })
+      }
+      renderPeliculas(peliculas) // Ejecucion de la funcion que se encargar de hacer cambios en la plantilla y agregar elementos al dom
+    })
+  }
+  $botonPeliculas.click(pedido) // Evento que ejecuta la funcion pedido
+})
